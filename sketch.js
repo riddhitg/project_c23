cab84e96626c99af66e83bc3717cc11467687e5c
riddhitg <61347574+riddhitg@users.noreply.github.com>
2020-03-28 22:37:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(1000,400);
+
+  createSprite(460, 200, 400, 10);
+  createSprite(265, 300, 10, 200);
+  createSprite(655, 300, 10, 200);
+  createSprite(135, 400, 265, 10);
+  createSprite(790, 400, 265, 10);
+  createSprite(200, 400, 10, 500);
+  createSprite(100, 400, 10, 500);
+  createSprite(150, 150, 200, 10);
+  createSprite(50, 105, 10, 100);
+  createSprite(250, 105, 10, 100);
+  createSprite(70, 50, 50, 10);
+  createSprite(100, 70, 10, 50);
+  createSprite(120, 100, 50, 10);
+  createSprite(140, 75, 10, 50);
+  createSprite(160, 50, 50, 10);
+  createSprite(180, 75, 10, 50);
+  createSprite(200, 95, 50, 10);
+  createSprite(220, 75, 10, 50);
+  createSprite(235, 50, 50, 10);
+  createSprite(750, 300, 10, 300);
+  createSprite(850, 300, 10, 300);
+  createSprite(800, 150, 250, 10);
+  createSprite(680, 100, 10, 100);
+  createSprite(920, 100, 10, 100);
+  createSprite(700, 50, 50, 10);
+  createSprite(720, 75, 10, 50);
+  createSprite(740, 100, 50, 10);
+  createSprite(760, 75, 10, 50);
+  createSprite(780, 50, 50, 10);
+  createSprite(800, 75, 10, 50);
+  createSprite(820, 100, 50, 10);
+  createSprite(840, 75, 10, 50);
+  createSprite(880, 50, 100, 10);
+  createSprite(300, 175, 10, 50);
+  createSprite(625, 175, 10, 50);
+  createSprite(462, 150, 335, 10);
+  createSprite(462, 100, 10, 100);
+  createSprite(508, 50, 100, 10);
+  createSprite(552, 75, 10, 50);
+  createSprite(508, 100, 100, 10);
+ 
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
